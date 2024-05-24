033f7ac366cecd32affe83829405929d1c3a6fe8
Dunn254 <124111950+Dunn254@users.noreply.github.com>
2024-05-23 20:27:03 -0700

    album/artist view

---
+++ src/components/albumView.js
@@ -0,0 +1,20 @@
+// These components will be making separate API calls from the app
+// component to serve specific data about a given album
+import { useParams } from 'react-router-dom'
+import { useState } from 'react'
+
+function AlbumView() {
+    const { id } = useParams()
+    const [ artistData, setArtistData ] = useState([])
+
+    return (
+        <div>
+            <h2>The id passed was: {id}</h2>
+            <p>Artist Data Goes Here!</p>
+        </div>
+    )
+}
+
+
+export default AlbumView
+
